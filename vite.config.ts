85a202d6c7c9d24fea9f19f86d9f5f681b369a3d
FalkZ <falk.dev@proton.me>
2024-05-02 23:30:10 +0200

    fixed node modules issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import runBuiltFiles from "./vite-run-built";
 export default defineConfig({
 	plugins: [dts(), runBuiltFiles()],
 	build: {
+		target: "node21",
 		sourcemap: "inline",
 		lib: {
 			entry: "./src/main.ts",
 			formats: ["es"],
 			fileName: "main",
 		},
+		ssr: true, // this is set so that vite doesn't complain about Node.js modules like fs, path, etc.
 	},
 	optimizeDeps: {
 		exclude: ["*"], // exclude all modules from being bundled
